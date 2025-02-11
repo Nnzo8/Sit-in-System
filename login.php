d1e3fb1cec1ed020936372c2d3a805a1b32b7ed5
Niño Rollane D. Ocliasa <ninorollaneocliasa684@gmail.com>
2025-02-11 09:39:34 +0800

    Updated background

--- login.php
+++ login.php
@@ -9,6 +9,11 @@ session_start();
     <title>CSS Sit-in Monitoring System</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <style>
+    body {
+        background: linear-gradient(to bottom, #291c0e, #e1d4c2);
+    }
+    </style>
 </head>
 <body>
     <div class="container">
--- registration.php
+++ registration.php
@@ -12,6 +12,11 @@ function validateText(event) {
     textInput.value = textInput.value.replace(/[^a-zA-Z\s]/g, '');
 }
 </script>
+<style>
+body {
+    background: linear-gradient(to bottom, #291c0e, #e1d4c2);
+}
+</style>
 </head>
 <body>
 <div class="container">
