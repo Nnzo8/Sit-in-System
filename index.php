4c9818e1024cc1122fda3bc40951f7b925123ab8
Niño Rollane D. Ocliasa <ninorollaneocliasa684@gmail.com>
2025-02-16 21:11:14 +0800

    Updated the containers in index

--- index.php
+++ index.php
@@ -136,14 +136,30 @@ document.addEventListener('click', function(event) {
     <!-- Rules and Regulations -->
     <div class="container">
         <div class="center-container">
-            <h5>Rules and Regulations</h5>
+            
+            <h5>University of Cebu
+                COLLEGE OF INFORMATION & COMPUTER STUDIES
+
+
+                LABORATORY RULES AND REGULATIONS</h5>
         </div>
-        <p>Please follow these guidelines while attending sit-in sessions:</p>
+        <p>To avoid embarrassment and maintain camaraderie with your friends and superiors at our laboratories, please observe the following:</p>
         <ul>
-            <li>Maintain silence in the classroom.</li>
-            <li>Be punctual.</li>
-            <li>Respect faculty and staff.</li>
-            <li>No food or drinks allowed inside the classroom.</li>
+            <li>1. Maintain silence, proper decorum, and discipline inside the laboratory. Mobile phones, walkmans and other personal pieces of equipment must be switched off.</li>
+            <li>2. Games are not allowed inside the lab. This includes computer-related games, card games and other games that may disturb the operation of the lab.</li>
+            <li>3. Surfing the Internet is allowed only with the permission of the instructor. Downloading and installing of software are strictly prohibited.</li>
+            <li>4. Getting access to other websites not related to the course (especially pornographic and illicit sites) is strictly prohibited.</li>
+            <li>5. Deleting computer files and changing the set-up of the computer is a major offense.</li>
+            <li>6. Observe computer time usage carefully. A fifteen-minute allowance is given for each use. Otherwise, the unit will be given to those who wish to "sit-in".</li>
+            <li>7. Observe proper decorum while inside the laboratory.</li>
+            <p>Do not get inside the lab unless the instructor is present.
+All bags, knapsacks, and the likes must be deposited at the counter.
+Follow the seating arrangement of your instructor.
+At the end of class, all software programs must be closed.
+Return all chairs to their proper places after using.</p>
+            <li>8. Chewing gum, eating, drinking, smoking, and other forms of vandalism are prohibited inside the lab.</li>
+            <li>9. Anyone causing a continual disturbance will be asked to leave the lab. Acts or gestures offensive to the members of the community, including public display of physical intimacy, are not tolerated.</li>
+            <li>10. Persons exhibiting hostile or threatening behavior such as yelling, swearing, or disregarding requests made by lab personnel will be asked to leave the lab.</li>
         </ul>
     </div>
 </div>
